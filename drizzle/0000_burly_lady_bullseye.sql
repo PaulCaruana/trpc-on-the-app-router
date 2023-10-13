43fedfa4863f884380c3d3d00935ef15e06f999d
paulcaruana <caruanapaul023@gmail.com>
2023-10-13 14:59:13 +1100

    Initial

---
+++ drizzle/0000_burly_lady_bullseye.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `todos` (
+	`id` integer PRIMARY KEY NOT NULL,
+	`content` text,
+	`done` integer
+);
+--> statement-breakpoint
+CREATE TABLE `users` (
+	`id` integer PRIMARY KEY NOT NULL,
+	`name` text,
+	`email` text
+);
